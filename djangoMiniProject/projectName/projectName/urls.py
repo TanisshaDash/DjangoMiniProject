0bf1ed1d40a5d6d5a1e25cecfe5fd61f30d2498f
TanisshaDash <tanissha.1610@gmail.com>
2025-09-22 19:11:59 +0530

    updates

--- djangoMiniProject/projectName/projectName/urls.py
+++ djangoMiniProject/projectName/projectName/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from projectName.views import hello_geeks 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("projectApp.urls")),
+    path('geek/', hello_geeks),
 ]
