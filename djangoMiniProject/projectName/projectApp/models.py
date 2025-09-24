c441ea4edc1fa6244a5f47762fc580937bfc00f9
TanisshaDash <tanissha.1610@gmail.com>
2025-09-24 20:34:30 +0530

    updates

--- djangoMiniProject/projectName/projectApp/models.py
+++ djangoMiniProject/projectName/projectApp/models.py
@@ -1,10 +1,15 @@
+# import the standard Django Model
+# from built-in library
 from django.db import models
-
+ 
+# declare a new model with a name "GeeksModel"
 class GeeksModel(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    last_modified = models.DateTimeField(auto_now_add=True)
-    img = models.ImageField(upload_to="images/")
 
+    # fields of the model
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+
+    # renames the instances of the model
+    # with their title name
     def __str__(self):
         return self.title
--- djangoMiniProject/projectName/projectApp/views.py
+++ djangoMiniProject/projectName/projectApp/views.py
@@ -1,6 +1,13 @@
-# importing HttResponse from library
+# import Http Response from django
 from django.http import HttpResponse
+# get datetime
+import datetime
 
-def home(request):
-    # request is handled using HttpResponse object
-    return HttpResponse("Any kindz of HTML Here")
+# create a function
+def geeks_view(request):
+    # fetch date and time
+    now = datetime.datetime.now()
+    # convert to string
+    html = "Time is {}".format(now)
+    # return response
+    return HttpResponse(html)
