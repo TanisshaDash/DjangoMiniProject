e75832346434fec2c52025dc0907bfb750e82d94
TanisshaDash <tanissha.1610@gmail.com>
2025-09-24 21:10:30 +0530

    updates

--- djangoMiniProject/projectName/projectApp/urls.py
+++ djangoMiniProject/projectName/projectApp/urls.py
@@ -6,3 +6,10 @@ from .views import list_view
 urlpatterns = [
     path('', list_view),
 ]
+from django.urls import path
+
+# importing views from views..py
+from .views import delete_view
+urlpatterns = [
+    path('<id>/delete', delete_view ),
+]
