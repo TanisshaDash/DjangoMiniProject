57c853676b61e0117d921ed7922f9a10d26091c0
TanisshaDash <tanissha.1610@gmail.com>
2025-09-24 20:49:45 +0530

    updates

--- djangoMiniProject/projectName/projectApp/urls.py
+++ djangoMiniProject/projectName/projectApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import geeks_view
+from .views import list_view
 
 urlpatterns = [
-    path('', geeks_view),
+    path('', list_view),
 ]
--- djangoMiniProject/projectName/projectApp/views.py
+++ djangoMiniProject/projectName/projectApp/views.py
@@ -1,13 +1,15 @@
-# import Http Response from django
-from django.http import HttpResponse
-# get datetime
-import datetime
+from django.shortcuts import render
 
-# create a function
-def geeks_view(request):
-    # fetch date and time
-    now = datetime.datetime.now()
-    # convert to string
-    html = "Time is {}".format(now)
-    # return response
-    return HttpResponse(html)
+# relative import of forms
+from .models import GeeksModel
+
+
+def list_view(request):
+    # dictionary for initial data with 
+    # field names as keys
+    context ={}
+
+    # add the dictionary during initialization
+    context["dataset"] = GeeksModel.objects.all()
+        
+    return render(request, "list_view.html", context)
