288c8828acb09eb9e2fe343509467cbaa84e679d
TanisshaDash <tanissha.1610@gmail.com>
2025-09-29 15:11:58 +0530

    loli

---
+++ djangoMiniProject/projectName/bookstore/mini/forms.py
@@ -0,0 +1,10 @@
+# mini/forms.py
+from django import forms
+from django.core.validators import URLValidator
+
+class URLForm(forms.Form):
+    url = forms.URLField(
+        label='Enter a URL',
+        validators=[URLValidator()],
+        widget=forms.TextInput(attrs={'placeholder': 'https://example.com/'})
+    )
