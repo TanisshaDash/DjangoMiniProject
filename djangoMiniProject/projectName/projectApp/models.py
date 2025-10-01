fa3e69bbb290d44ce6251239d910948108fe38b9
TanisshaDash <tanissha.1610@gmail.com>
2025-10-01 15:55:54 +0530

    migrations

--- djangoMiniProject/projectName/projectApp/models.py
+++ djangoMiniProject/projectName/projectApp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class GeeksModel(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
+class FacultyDetails(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    department = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.title
+        return f"{self.first_name} {self.last_name}"
