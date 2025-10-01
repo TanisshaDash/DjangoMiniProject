ad5224049aa5e4f7fd191dcc0f2527b2a7f17930
TanisshaDash <tanissha.1610@gmail.com>
2025-10-01 16:02:00 +0530

    migrations

--- djangoMiniProject/projectName/projectApp/admin.py
+++ djangoMiniProject/projectName/projectApp/admin.py
@@ -2,3 +2,14 @@ from django.contrib import admin
 from .models import FacultyDetails
 
 admin.site.register(FacultyDetails)
+
+from django.contrib import admin
+
+from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'isbn', 'available')
+    search_fields = ('title', 'author', 'isbn')
+    list_filter = ('available', 'published_date')
