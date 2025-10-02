b32b42ca793947c9907176103f3be54bbad3b291
TanisshaDash <tanissha.1610@gmail.com>
2025-10-02 12:45:01 +0530

    rest api work

--- djangoMiniProject/projectName/projectApp/models.py
+++ djangoMiniProject/projectName/projectApp/models.py
@@ -44,3 +44,14 @@ class GeeksModel(models.Model):
         # with their title name
     def __str__(self):
         return self.title
+    
+#rest based book model 
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publish_date = models.DateField()
+
+    def __str__(self):
+        return self.title
