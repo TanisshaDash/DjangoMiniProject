06fea01fe3dd2170df221b250dc925b0f2d951b4
TanisshaDash <tanissha.1610@gmail.com>
2025-10-07 19:21:03 +0530

    learning-django framework w/ sql integration

--- djangoMiniProject/projectName/projectApp/views.py
+++ djangoMiniProject/projectName/projectApp/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
