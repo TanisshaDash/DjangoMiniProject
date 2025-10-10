2cd8ff28ab4206592dabb8c3cb0bd6f1eaddc2de
TanisshaDash <tanissha.1610@gmail.com>
2025-10-10 15:25:47 +0530

    user

--- djangoMiniProject/miniproject/gfg_auth_project/manage.py
+++ djangoMiniProject/miniproject/gfg_auth_project/manage.py
@@ -1,20 +1,23 @@
 import os
 import sys
-
+from dotenv import load_dotenv  # make sure to install python-dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gfg_auth_project.settings')
+    load_dotenv()  # load variables from .env file
+
+    # Allow HTTP OAuth in dev only
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = os.getenv('OAUTHLIB_INSECURE_TRANSPORT', '0')
+
+    # Point to your credentials file securely
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError("Couldn't import Django.") from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
