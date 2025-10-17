37b47e8c68194d84a80d843461ba736a50ea2e1d
TanisshaDash <tanissha.1610@gmail.com>
2025-10-17 19:12:07 +0530

    project

--- djangoMiniProject/miniproject/gfg_auth_project/gfg_auth_project/urls.py
+++ djangoMiniProject/miniproject/gfg_auth_project/gfg_auth_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from gmail_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gmail/', include('gmail_app.urls')),
+    path('', views.home, name='home'),
 ]
