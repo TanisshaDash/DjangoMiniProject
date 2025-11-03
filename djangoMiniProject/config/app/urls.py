91ce7f461f4ba197713c35e5f1a9888503cf79c2
TanisshaDash <tanissha.1610@gmail.com>
2025-11-03 19:22:46 +0530

    jwt auth test

--- djangoMiniProject/config/app/urls.py
+++ djangoMiniProject/config/app/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from app import views  # ✅ use your app name here
 
 urlpatterns = [
-    path('hello/', views.HelloView.as_view(), name ='hello'),
+    path('admin/', admin.site.urls),
+    path('api/token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', views.MyTokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', views.hello_view, name='hello'),
 ]
--- djangoMiniProject/config/app/views.py
+++ djangoMiniProject/config/app/views.py
@@ -1,11 +1,8 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-
-class HelloView(APIView):
-    permission_classes = (IsAuthenticated, )
-
-    def get(self, request):
-        content = {'message': 'Hello, GeeksforGeeks'}
-        return Response(content)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def hello_view(request):
+    return Response({"message": "Hello, user!"})
